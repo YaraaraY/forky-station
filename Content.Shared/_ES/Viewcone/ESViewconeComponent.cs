28113642e90a5d21ff4748941aba32b62db1f2ea
mirrorcult <mirrorcult@pm.me>
2025-11-26 16:16:29 +0000

    mild viewcone change (#312)

--- Content.Shared/_ES/Viewcone/ESViewconeComponent.cs
+++ Content.Shared/_ES/Viewcone/ESViewconeComponent.cs
@@ -35,11 +35,18 @@ public sealed partial class ESViewconeComponent : Component
     public float ConeAngle = 225f;
 
     [DataField, AutoNetworkedField]
-    public float ConeFeather = 10f;
+    public float ConeFeather = 5f;
 
     [DataField, AutoNetworkedField]
-    public float ConeIgnoreRadius = 0.85f;
+    public float ConeIgnoreRadius = 0.7f;
 
     [DataField, AutoNetworkedField]
-    public float ConeIgnoreFeather = 0.25f;
+    public float ConeIgnoreFeather = 0.1f;
+
+    // Clientside, used for lerping view angle
+    // and keeping it consistent across all overlays
+    public Angle ViewAngle;
+    public Angle? DesiredViewAngle = null;
+    public Angle LastMouseRotationAngle;
+    public Angle LastWorldRotationAngle;
 }
