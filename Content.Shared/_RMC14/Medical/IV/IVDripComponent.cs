4726c7071cb43edbee6a01df8b902950665efc47
MaiaArai <158123176+YaraaraY@users.noreply.github.com>
2025-12-18 02:16:34 -0300

    IV Stand Resprite (#2345)
    * IV Resprite
    
    * one day i'll get the licenses right in the first try
    
    * chore: Automatically update REUSE headers
    
    * Revert "chore: Automatically update REUSE headers"
    
    This reverts commit d75ac6c173817d0dddb58b13ceeebaa75455bc00.
    
    * chore: Automatically update REUSE headers
    
    ---------
    
    Co-authored-by: funkystationbot <funky@funkystation.org>

--- Content.Shared/_RMC14/Medical/IV/IVDripComponent.cs
+++ Content.Shared/_RMC14/Medical/IV/IVDripComponent.cs
@@ -5,6 +5,7 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using System.Numerics;
 
 namespace Content.Shared._RMC14.Medical.IV;
 
@@ -32,6 +33,9 @@ public sealed partial class IVDripComponent : Component
     [DataField, AutoNetworkedField]
     public string UnattachedState = "unhooked";
 
+    [DataField, AutoNetworkedField]
+    public string NoBagState = "empty";
+
     /// <summary>
     ///     Percentages are from 0 to 100
     /// </summary>
@@ -58,6 +62,12 @@ public sealed partial class IVDripComponent : Component
 
     [DataField, AutoNetworkedField]
     public ProtoId<EmotePrototype> RipEmote = "Scream";
+
+    /// <summary>
+    /// Offset from the center of the entity where the IV line should start
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public Vector2 LineOriginOffset = Vector2.Zero;
 }
 
 [Serializable, NetSerializable]
