d3ac5a12738d3ae224234f0948bfc76811062682
mirrorcult <mirrorcult@pm.me>
2026-02-12 22:40:35 +0000

    Screenshake (#627)
    * commit your shit at some point maybe
    
    * y
    
    * who knows
    
    * rename?
    
    * debugging  + shit api
    
    * i hope no one fucks my ass
    
    * erm
    
    * p
    
    * replace with shit that makes way more sense
    
    * stuff
    
    * pretty sure this is just fine
    
    * actual fix noise shit
    
    * uhhh hmaybe if i feel like it wrong then later
    
    * explosion shake
    
    * grav shake
    
    * gun shake
    
    * idk
    
    * projectile melee

--- Content.Shared/Gravity/SharedGravitySystem.Shake.cs
+++ Content.Shared/Gravity/SharedGravitySystem.Shake.cs
@@ -1,8 +1,16 @@
+using Content.Shared._ES.Camera;
+using Content.Shared.GameTicking;
+using Robust.Shared.Player;
+
 namespace Content.Shared.Gravity;
 
 public abstract partial class SharedGravitySystem
 {
     [Dependency] private EntityQuery<GravityComponent> _gravityQuery = default!;
+    // ES START
+    [Dependency] private SharedGameTicker _ticker = default!;
+    [Dependency] private ESScreenshakeSystem _shake = default!;
+    // ES END
 
     protected const float GravityKick = 100.0f;
     protected const float ShakeCooldown = 0.2f;
@@ -38,6 +46,20 @@ public abstract partial class SharedGravitySystem
         if (!Resolve(uid, ref gravity, false))
             return;
 
+        // ES START
+        // do not shake grid if the round just started
+        // i did not want to have to think this logic through more. this is the simplest solution i could think of
+        if (Timing.CurTime - _ticker.RoundStartTimeSpan < TimeSpan.FromSeconds(30))
+            return;
+
+        // ES SCREENSHAKE LOGIC
+        // instead of poopass camera kick
+        var translation = new ESScreenshakeParameters() { Trauma = 0.8f, DecayRate = 0.04f, Frequency = 0.015f };
+        var filter = Filter.BroadcastGrid(uid);
+        _shake.Screenshake(filter, translation, null);
+
+        return;
+        // ES END
         if (!TryComp<GravityShakeComponent>(uid, out var shake))
         {
             shake = AddComp<GravityShakeComponent>(uid);
