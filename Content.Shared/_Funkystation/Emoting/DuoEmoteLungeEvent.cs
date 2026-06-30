0f3f9d6f344c70c25b4981228518b2a9d9875de1
YaraaraY <158123176+YaraaraY@users.noreply.github.com>
2026-06-30 18:35:22 -0300

    duo emotes

---
+++ Content.Shared/_Funkystation/Emoting/DuoEmoteLungeEvent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._Funkystation.Emoting;
+
+/// <summary>
+/// Raised on clients to trigger lunge animations for both participants
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class DuoEmoteLungeEvent(NetEntity initiator, NetEntity partner) : EntityEventArgs
+{
+    public NetEntity Initiator { get; } = initiator;
+    public NetEntity Partner { get; } = partner;
+}
