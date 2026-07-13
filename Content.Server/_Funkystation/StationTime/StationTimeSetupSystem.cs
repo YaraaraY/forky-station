b36a0ca2add0067804993da93ca41c90d03a2275
YaraaraY <158123176+YaraaraY@users.noreply.github.com>
2026-07-12 23:10:56 -0300

    GOD PLEASE

--- Content.Server/_Funkystation/StationTime/StationTimeSetupSystem.cs
+++ Content.Server/_Funkystation/StationTime/StationTimeSetupSystem.cs
@@ -1,5 +1,5 @@
-﻿using Content.Server.Station.Systems;
-using Content.Shared._Funkystation.StationTime.Components;
+﻿using Content.Shared._Funkystation.StationTime.Components;
+using Content.Shared.Station.Components;
 using Robust.Shared.Timing;
 
 namespace Content.Server._Funkystation.StationTime;
@@ -8,17 +8,19 @@ public sealed partial class StationTimeSetupSystem : EntitySystem
 {
     [Dependency] private IGameTiming _timing = null!;
 
-    public override void Initialize()
+    public override void Update(float frameTime)
     {
-        base.Initialize();
-        SubscribeLocalEvent<StationInitializedEvent>(OnStationInitialized);
-    }
+        base.Update(frameTime);
 
-    private void OnStationInitialized(StationInitializedEvent ev)
-    {
-        var comp = EnsureComp<StationTimeComponent>(ev.Station);
-        comp.RealUtcTicksAtSync = DateTime.UtcNow.Ticks;
-        comp.CurTimeAtSync = _timing.CurTime;
-        Dirty(ev.Station, comp);
+        var query = EntityQueryEnumerator<StationDataComponent>();
+        while (query.MoveNext(out var uid, out _))
+        {
+            if (HasComp<StationTimeComponent>(uid))
+                continue;
+
+            var comp = AddComp<StationTimeComponent>(uid);
+            comp.RealUtcTicksAtSync = DateTime.UtcNow.Ticks;
+            comp.CurTimeAtSync = _timing.CurTime;
+        }
     }
 }
