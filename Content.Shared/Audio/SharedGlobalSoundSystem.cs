cf7ba9a511ad8352268d47adbcd3a43598a69b3d
YaraaraY <158123176+YaraaraY@users.noreply.github.com>
2026-08-05 00:03:38 -0300

    Revert "Merge pull request #17 from Terkala/BloodCult-Port"
    This reverts commit a86aca36c0155ed99d5a9a6e5e6f2b0be78e35aa, reversing
    changes made to a0a5c703a445075e7daba7158fe8bcaa5fdfa314.

--- Content.Shared/Audio/SharedGlobalSoundSystem.cs
+++ Content.Shared/Audio/SharedGlobalSoundSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 ike709 <ike709@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 pathetic meowmeow <uhhadd@gmail.com>
+// SPDX-License-Identifier: MIT
+
 using Content.Shared.CCVar;
 using Robust.Shared.Audio;
 using Robust.Shared.Serialization;
@@ -43,8 +47,7 @@ public sealed class GameGlobalSoundEvent : GlobalSoundEvent
 
 public enum StationEventMusicType : byte
 {
-    Nuke,
-    BloodCult
+    Nuke
 }
 
 /// <summary>
