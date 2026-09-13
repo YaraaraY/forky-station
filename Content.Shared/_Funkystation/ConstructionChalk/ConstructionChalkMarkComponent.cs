2c77c93a0c0a25102ad3907db15575f790f2d52a
YaraaraY <158123176+YaraaraY@users.noreply.github.com>
2026-09-12 23:56:44 -0300

    lint

--- Content.Shared/_Funkystation/ConstructionChalk/ConstructionChalkMarkComponent.cs
+++ Content.Shared/_Funkystation/ConstructionChalk/ConstructionChalkMarkComponent.cs
@@ -10,7 +10,7 @@ namespace Content.Shared._Funkystation.ConstructionChalk;
 public sealed partial class ConstructionChalkMarkComponent : Component
 {
     [DataField, AutoNetworkedField]
-    public ProtoId<ConstructionPrototype> ConstructionPrototype = default!;
+    public ProtoId<ConstructionPrototype> ConstructionPrototype = new(string.Empty);
 
     [DataField, AutoNetworkedField]
     public Angle Rotation = Angle.Zero;
